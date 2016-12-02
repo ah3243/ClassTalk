c229a1c00e34f3eb58e8159a80db856e35484931
Albert Higgins <1c2fffe2@opayq.com>
2016-12-02 12:12:57 +0000

    breaklines

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -11,9 +11,9 @@ export class TeacherComponent implements OnInit {
 
   // Variables for incrementing the words on click
   inputScript: string = `Hello! I thought that I would share with you some of the materials I’m really into at the moment because the Institute of Making is about the relationship between materials and processes - so this began in some respects it's a life's work.
-<br/><br/>
+
 It began quite formally at UCL about 10 years ago when we brought our materials library - which myself and the other director Nigel Martin have been building for about the last 10 years. We wanted to celebrate the relationship between materials and process because these things are together here at UCL. It's essentially floor to ceiling cabinets and curious stuff - it's not like a museum. Everything in there is handleable and we have to accept that things might get broken. But in breaking things you learn about materials and you might want to take something down to the workshop to fix it. We are interested in the whole journey of the materials and what we get up to when we handle them - that also constitutes making. I guess today I want to really concentrate on some of my favourite materials of the moment and talk to you about them and show them to you.
-<br/><br/>
+
 I suppose one of the important sort of things in material science is that notion that inside stuff, inside matter, there are structures. And those structures are organised in such a way that they give properties to the things. The ability for a steel bar to be stiff is all about the internal structure and the arrangement of both the atoms and the crystals inside that. And you might think ‘well what do I mean by the word crystals?’, because culturally we think of crystals as the stuff that we might find in a rock or in a gem shop. Some people might think of ice crystals or something that you shine a light through. But you definitely don't think of and you don't use the word crystals to think of metal. But in fact all metals are made of crystals and it's the arrangement and configurations and shape and size of those crystals that give lots of properties.
 `;
   script: string[] = [];
